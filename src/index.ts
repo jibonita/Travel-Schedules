22b5a6034b1921340ef94dd9e539e8dd0a3fd4e5
Osman Yumer <osman.yumer@abv.bg>
2018-12-10 15:42:24 +0200

    adding simple table

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import 'reflect-metadata';
+import {createConnection} from 'typeorm';
+import {Usertype} from './entity/usertype';
+
+createConnection().then(async connection => {
+
+
+    // const user = new UserType();
+    // user.name = 'Admin';
+    // await connection.manager.save(user);
+    // console.log('Saved a new user with id: ' + user.UserTypeID);
+    // const users = await connection.manager.find(UserType);
+    // console.log('Loaded users: ', users);
+
+    
+}).catch(error => console.log(error));
