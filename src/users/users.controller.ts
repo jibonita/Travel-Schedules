cc4566dcbd393fc7d9538e7f4b5218bcd441cf76
Osman Yumer <osman.yumer@abv.bg>
2018-12-19 15:09:31 +0200

    added delete user route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,9 @@
+import { AddRouteDTO } from './../models/route/add-route.dto';
 import { AdminGuard } from './../common/guards/roles/admin.guard';
 import { AuthGuard } from '@nestjs/passport';
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards, Delete, Param, Body, ValidationPipe } from '@nestjs/common';
 import { UsersService } from './../common/core/users.service';
+import { GetUserDTO } from '../models/user/get-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -11,8 +13,16 @@ export class UsersController {
   ) { }
 
   @Get()
-  @UseGuards(AuthGuard(), AdminGuard)
   all() {
     return this.usersService.getAll();
+  }
+
+  @Delete()
+  findOne(@Body(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+  }))user: GetUserDTO ) {
+    console.log(user);
+    return this.usersService.deleteUser(user);
   }
 }
