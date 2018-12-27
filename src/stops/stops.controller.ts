195d3c955d34d11d5c021f02c33ec261f4d6d630
jibonita <bamishore@gmail.com>
2018-12-27 19:41:46 +0200

    added guards to stops controller

--- src/stops/stops.controller.ts
+++ src/stops/stops.controller.ts
@@ -1,18 +1,24 @@
 import { StopsService } from './stops.service';
-import { Controller, Get, Body, Post, ValidationPipe } from '@nestjs/common';
+import { Controller, Get, Body, Post, ValidationPipe, UseGuards } from '@nestjs/common';
 import { AddStopDTO } from '../models/stop/add-stop.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { Roles } from '../common/decorators/roles.decorator';
 
 @Controller('stops')
+@UseGuards(AuthGuard())
+
 export class StopsController {
     constructor(
         private readonly stopsService: StopsService,
       ) { }
 
     @Get()
+    @Roles('admin')
     all() {
         return this.stopsService.getAll();
     }
 
+    @Roles('admin', 'company')
     @Post()
     async add(
         @Body(new ValidationPipe({
