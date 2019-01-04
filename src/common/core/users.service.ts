1272813307314819fbd0a24ee7ef85bdb4390cbd
jibonita <bamishore@gmail.com>
2019-01-04 15:13:06 +0200

    renames UserService singIn method to signInUser

--- src/common/core/users.service.ts
+++ src/common/core/users.service.ts
@@ -40,7 +40,7 @@ export class UsersService {
     return userFound;
   }
 
-  async signIn(user: UserLoginDTO): Promise<GetUserDTO> {
+  async signInUser(user: UserLoginDTO): Promise<GetUserDTO> {
     const userFound: GetUserDTO = await this.usersRepository
         .findOne({ select: ['email', 'password', 'usertype'],
                   where: { email: user.email } });
